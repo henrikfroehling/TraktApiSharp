6940621b8f4c942d74dcf9f7ea1c0472b3ede01b
Henrik Fröhling <henrik.froehling@gmail.com>
2017-05-13 08:52:47 +0200

    Add certifications request implementation

---
+++ Source/Lib/TraktApiSharp/Requests/WithoutOAuth/Certifications/ATraktCertificationsRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace TraktApiSharp.Requests.WithoutOAuth.Certifications
+{
+    using Base.Get;
+    using Objects.Basic;
+
+    internal abstract class ATraktCertificationsRequest : TraktGetRequest<TraktCertifications, TraktCertifications>
+    {
+        internal ATraktCertificationsRequest(TraktClient client) : base(client) { }
+
+        protected override TraktAuthorizationRequirement AuthorizationRequirement => TraktAuthorizationRequirement.NotRequired;
+    }
+}
---
+++ Source/Lib/TraktApiSharp/Requests/WithoutOAuth/Certifications/TraktMovieCertificationsRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TraktApiSharp.Requests.WithoutOAuth.Certifications
+{
+    internal class TraktMovieCertificationsRequest : ATraktCertificationsRequest
+    {
+        internal TraktMovieCertificationsRequest(TraktClient client) : base(client) { }
+
+        protected override string UriTemplate => "certifications/movies";
+    }
+}
