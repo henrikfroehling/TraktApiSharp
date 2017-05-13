45114c7a5d57c959249405d65a2c3e82eb5fc9ff
Henrik Fröhling <henrik.froehling@gmail.com>
2017-05-13 08:40:22 +0200

    Add certification object implementation

---
+++ Source/Lib/TraktApiSharp/Objects/Basic/TraktCertification.cs
@@ -0,0 +1,24 @@
+﻿namespace TraktApiSharp.Objects.Basic
+{
+    using Attributes;
+    using Newtonsoft.Json;
+
+    /// <summary>A Trakt certification.</summary>
+    public class TraktCertification
+    {
+        /// <summary>Gets or sets the certification name.<para>Nullable</para></summary>
+        [JsonProperty(PropertyName = "name")]
+        [Nullable]
+        public string Name { get; set; }
+
+        /// <summary>Gets or sets the certification slug.<para>Nullable</para></summary>
+        [JsonProperty(PropertyName = "slug")]
+        [Nullable]
+        public string Slug { get; set; }
+
+        /// <summary>Gets or sets the certification description.<para>Nullable</para></summary>
+        [JsonProperty(PropertyName = "description")]
+        [Nullable]
+        public string Description { get; set; }
+    }
+}
